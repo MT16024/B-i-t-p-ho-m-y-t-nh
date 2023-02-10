84c5eb534e551bf42da3da82d9c0e55340d3ae35
MT16024 <101939561+MT16024@users.noreply.github.com>
2023-02-10 21:01:51 +0700

    Update Line-1.cpp

--- Line-1.cpp
+++ Line-1.cpp
@@ -45,7 +45,6 @@ void mydisplay()
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 0.0, 0.0);  
     BreLine(10, 10, 200, 100);
-//    MidLine(10, 10, 200, 100);
     glViewport(0,0,640,480);
     glFlush();
 }
