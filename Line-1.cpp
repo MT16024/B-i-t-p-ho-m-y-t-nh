3bf05cdb786a7e38be6e3ab6b7e54f4044da453f
MT16024 <101939561+MT16024@users.noreply.github.com>
2023-02-10 20:58:32 +0700

    Update Line-1.cpp

--- Line-1.cpp
+++ Line-1.cpp
@@ -7,8 +7,9 @@ void initGL()
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); 
 	glOrtho(-320,320,-240,240,-1,1);	
 }
+
 void BreLine(int x1,int y1,int x2,int y2)   
-//he so goc 0<m<1
+	//0<m<1
 {
     int x_max,x,y;
      int dx=fabs(x1-x2);
