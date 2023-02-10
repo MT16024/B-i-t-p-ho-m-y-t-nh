84fd062ee9b4eb0174b99ac294b906a6b353c0e1
MT16024 <101939561+MT16024@users.noreply.github.com>
2023-02-10 21:07:59 +0700

    Update Line-2.cpp

--- Line-2.cpp
+++ Line-2.cpp
@@ -12,22 +12,26 @@ void BreLine(int x1, int y1, int x2, int y2)
 	//y1=y2
 {
     int x_min, x_max;
-    if (x1 > x2) {
+    if (x1 > x2) 
+    {
         x_min = x2;
         x_max = x1;
     }
-    else {
+    else 
+    {
         x_min = x1;
         x_max = x2;
     }
+	
     glBegin(GL_POINTS);
-    for (int x = x_min; x <= x_max; x++) {
+    for (int x = x_min; x <= x_max; x++) 
+    {
         glVertex2i(x, y1);
     }
     glEnd();
 }
 
-     void mydisplay()
+void mydisplay()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 0.0, 0.0); 
@@ -35,6 +39,7 @@ void BreLine(int x1, int y1, int x2, int y2)
     glViewport(0,0,640,480);
     glFlush();
 }
+
 int main(int argc, char** argv){
     int mode=GLUT_SINGLE | GLUT_RGB;
     glutInitDisplayMode(mode);
